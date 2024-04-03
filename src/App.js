cf08bd8e4bbe986f47ce5041e2f709cbfe40e4a5
Panchammmm <sardarpancham510510@gmail.com>
2024-04-04 01:50:45 +0530

    about-us

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import NavMenu from './components/NavMenu';
 // import CardSlider from './components/CardSlider';
 import Banner from './components/Banner';
+import AboutUs from './components/AboutUs';
 
 export default function App() {
     return (
         <>
             <NavMenu />
             <Banner />
+            <AboutUs />
             {/* <CardSlider /> */}
         </>
     );
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './aboutus.css';
+
+export default function AboutUs() {
+    return (
+        <div className="about-container">
+            <h1>About Us</h1>
+            <span></span>
+            <p>Yume Japanese Language School, nestled in South Kolkata, is dedicated to providing exceptional Japanese language education. With experienced instructors and a comprehensive curriculum, we offer personalized learning experiences for students of all levels. Our mission is to equip students with not only language proficiency but also cultural insights, empowering them to succeed in a globalized world. Join us at Yume Japanese Language School and embark on a transformative journey towards linguistic mastery and cultural enrichment.</p>
+        </div>
+    );
+}
