97305a8888eef1f7db091ffb63dbfffd320e262a
Panchammmm <sardarpancham510510@gmail.com>
2024-04-04 13:58:32 +0530

    tailwind-implementation

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import './banner.css';
 import bannerImage from "../A_media/banner-image.png";
 
-const year = new Date().getFullYear();
+// const year = new Date().getFullYear();
 
 export default function Banner() {
     return (
         <>
             <div className="banner-container">
-                <img src={bannerImage}></img>
+                <img src={bannerImage} alt="banner"></img>
                 {/* <div className="display-text">OPEN FOR {year}</div> */}
             </div>
         </>
