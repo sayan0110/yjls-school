aa6bc27fd7fd569d7cf426c85569cd16579a023a
Panchammmm <sardarpancham510510@gmail.com>
2024-04-12 00:55:23 +0530

    UI-updated & footer

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import './banner.css';
-import bannerImage from "../A_media/banner-image.png";
-
-// const year = new Date().getFullYear();
+import bannerImage from "../A_media/banner-image1.png";
 
 export default function Banner() {
     return (
         <>
             <div className="banner-container">
                 <img src={bannerImage} alt="banner"></img>
-                {/* <div className="display-text">OPEN FOR {year}</div> */}
             </div>
         </>
     );
